6078b8b8db4895560053232ab8eaab9297f95fdf
Nobat <nobatgeldi@covisart.com>
2019-04-18 23:07:52 +0300

    send icon updated

--- samples/src/main/java/com/covisart/bekci/DecoderActivity.java
+++ samples/src/main/java/com/covisart/bekci/DecoderActivity.java
@@ -2,18 +2,23 @@ package com.covisart.bekci;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Point;
 import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
+
 import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
 import com.dlazaro66.qrcodereaderview.QRCodeReaderView.OnQRCodeReadListener;
 
@@ -23,143 +28,157 @@ import java.util.Date;
 
 public class DecoderActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback, OnQRCodeReadListener {
 
-  private static final int MY_PERMISSION_REQUEST_CAMERA = 0;
+    private static final int MY_PERMISSION_REQUEST_CAMERA = 0;
 
-  private ViewGroup mainLayout;
+    private ViewGroup mainLayout;
 
-  private TextView resultTextView;
-  private QRCodeReaderView qrCodeReaderView;
-  private CheckBox flashlightCheckBox;
-  private CheckBox enableDecodingCheckBox;
-  private PointsOverlayView pointsOverlayView;
-  private BackgroundMail bm;
+    private TextView resultTextView;
+    private QRCodeReaderView qrCodeReaderView;
+    private CheckBox flashlightCheckBox;
+    private CheckBox enableDecodingCheckBox;
+    private PointsOverlayView pointsOverlayView;
+    private BackgroundMail bm;
+    private Display display;
+    private Point size;
+    private Button mailButton;
 
-  @Override protected void onCreate(Bundle savedInstanceState)
-  {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_decoder);
-
-    mainLayout = (ViewGroup) findViewById(R.id.main_layout);
-    bm = new BackgroundMail(this);
-    bm.setGmailUserName("destek@covisart.com");
-    bm.setGmailPassword("aysenobat");
-    bm.setMailTo("nobatgeldi@outlook.com");
-    bm.setFormSubject("Bekci");
-    bm.setFormBody("Bekci Denetimi");
-    bm.setSendingMessage("Mesaj Gönderiliyor");
-    bm.setSendingMessageSuccess("Mesaj Gönderildi");
-
-    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED)
-    {
-      initQRCodeReaderView();
-    }
-    else
-    {
-      requestCameraPermission();
-    }
-  }
-
-  @Override protected void onResume() {
-    super.onResume();
-
-    if (qrCodeReaderView != null) {
-      qrCodeReaderView.startCamera();
-    }
-  }
-
-  @Override protected void onPause() {
-    super.onPause();
-
-    if (qrCodeReaderView != null) {
-      qrCodeReaderView.stopCamera();
-    }
-  }
-
-  @Override public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-      @NonNull int[] grantResults) {
-    if (requestCode != MY_PERMISSION_REQUEST_CAMERA) {
-      return;
+    public final static boolean isValidEmail(CharSequence target) {
+        return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
     }
 
-    if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-      Snackbar.make(mainLayout, "Camera permission was granted.", Snackbar.LENGTH_SHORT).show();
-      initQRCodeReaderView();
-    } else {
-      Snackbar.make(mainLayout, "Camera permission request was denied.", Snackbar.LENGTH_SHORT)
-          .show();
-    }
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_decoder);
 
-  // Called when a QR is decoded
-  // "text" : the text encoded in QR
-  // "points" : points where QR control points are placed
-  @Override public void onQRCodeRead(String text, PointF[] points) {
+        mainLayout = findViewById(R.id.main_layout);
+        bm = new BackgroundMail(this);
+        bm.setGmailUserName("bekci@covisart.com");
+        bm.setGmailPassword("*Nobat21*");
+        bm.setMailTo("nobatgeldi@outlook.com");
+        bm.setFormSubject("Bekci");
+        bm.setFormBody("Bekci Denetimi");
+        bm.setSendingMessage("Mesaj Gönderiliyor");
+        bm.setSendingMessageSuccess("Mesaj Gönderildi");
 
-
-    resultTextView.setText(text);
-      bm.setMailTo(text);
-    //bm.setFormBody("Bekci Geldi Gitti:" + text);
-    pointsOverlayView.setPoints(points);
-  }
-
-  private void requestCameraPermission() {
-    if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-      Snackbar.make(mainLayout, "Camera access is required to display the camera preview.",
-          Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
-        @Override public void onClick(View view) {
-          ActivityCompat.requestPermissions(DecoderActivity.this, new String[] {
-              Manifest.permission.CAMERA
-          }, MY_PERMISSION_REQUEST_CAMERA);
+        display = getWindowManager().getDefaultDisplay();
+        size = new Point();
+        display.getSize(size);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            initQRCodeReaderView();
+        } else {
+            requestCameraPermission();
         }
-      }).show();
-    } else {
-      Snackbar.make(mainLayout, "Permission is not available. Requesting camera permission.",
-          Snackbar.LENGTH_SHORT).show();
-      ActivityCompat.requestPermissions(this, new String[] {
-          Manifest.permission.CAMERA
-      }, MY_PERMISSION_REQUEST_CAMERA);
     }
-  }
 
-  private void initQRCodeReaderView() {
-    View content = getLayoutInflater().inflate(R.layout.content_decoder, mainLayout, true);
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-    qrCodeReaderView = (QRCodeReaderView) content.findViewById(R.id.qrdecoderview);
-    resultTextView = (TextView) content.findViewById(R.id.result_text_view);
-    flashlightCheckBox = (CheckBox) content.findViewById(R.id.flashlight_checkbox);
-    enableDecodingCheckBox = (CheckBox) content.findViewById(R.id.enable_decoding_checkbox);
-    pointsOverlayView = (PointsOverlayView) content.findViewById(R.id.points_overlay_view);
-
-    qrCodeReaderView.setAutofocusInterval(2000L);
-    qrCodeReaderView.setOnQRCodeReadListener(this);
-    qrCodeReaderView.setBackCamera();
-    flashlightCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-      @Override public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-        qrCodeReaderView.setTorchEnabled(isChecked);
-      }
-    });
-    enableDecodingCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-      @Override public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-        qrCodeReaderView.setQRDecodingEnabled(isChecked);
-      }
-    });
-    qrCodeReaderView.startCamera();
-  }
-
-  public void SendMail(View v)
-  {
-    try
-    {
-        Date c = Calendar.getInstance().getTime();
-        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy kk:mm:ss");
-        String formattedDate = df.format(c);
-
-        bm.setFormBody("Bekci Denetimi: " + formattedDate);
-        bm.send();
+        if (qrCodeReaderView != null) {
+            qrCodeReaderView.startCamera();
+        }
     }
-    catch (Exception exp)
-    {
-        Log.println(Log.ERROR,"Exp:",exp.toString());
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (qrCodeReaderView != null) {
+            qrCodeReaderView.stopCamera();
+        }
     }
-  }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode != MY_PERMISSION_REQUEST_CAMERA) {
+            return;
+        }
+
+        if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Snackbar.make(mainLayout, "Camera permission was granted.", Snackbar.LENGTH_SHORT).show();
+            initQRCodeReaderView();
+        } else {
+            Snackbar.make(mainLayout, "Camera permission request was denied.", Snackbar.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    // Called when a QR is decoded
+    // "text" : the text encoded in QR
+    // "points" : points where QR control points are placed
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+        resultTextView.setText(text);
+        if (isValidEmail(text)) {
+            bm.setMailTo(text);
+            mailButton.setVisibility(View.VISIBLE);
+        } else {
+            mailButton.setVisibility(View.INVISIBLE);
+        }
+        //bm.setFormBody("Bekci Geldi Gitti:" + text);
+        pointsOverlayView.setPoints(points);
+    }
+
+    private void requestCameraPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            Snackbar.make(mainLayout, "Camera access is required to display the camera preview.",
+                    Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ActivityCompat.requestPermissions(DecoderActivity.this, new String[]{
+                            Manifest.permission.CAMERA
+                    }, MY_PERMISSION_REQUEST_CAMERA);
+                }
+            }).show();
+        } else {
+            Snackbar.make(mainLayout, "Permission is not available. Requesting camera permission.",
+                    Snackbar.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.CAMERA
+            }, MY_PERMISSION_REQUEST_CAMERA);
+        }
+    }
+
+    private void initQRCodeReaderView() {
+        View content = getLayoutInflater().inflate(R.layout.content_decoder, mainLayout, true);
+
+        qrCodeReaderView = content.findViewById(R.id.qrdecoderview);
+        resultTextView = content.findViewById(R.id.result_text_view);
+        flashlightCheckBox = content.findViewById(R.id.flashlight_checkbox);
+        enableDecodingCheckBox = content.findViewById(R.id.enable_decoding_checkbox);
+        pointsOverlayView = content.findViewById(R.id.points_overlay_view);
+        mailButton = content.findViewById(R.id.button);
+
+        qrCodeReaderView.setAutofocusInterval(2000L);
+        qrCodeReaderView.setOnQRCodeReadListener(this);
+        qrCodeReaderView.setBackCamera();
+
+        flashlightCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                qrCodeReaderView.setTorchEnabled(isChecked);
+            }
+        });
+        enableDecodingCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                qrCodeReaderView.setQRDecodingEnabled(isChecked);
+            }
+        });
+        qrCodeReaderView.startCamera();
+    }
+
+    public void SendMail(View v) {
+        try {
+            Date c = Calendar.getInstance().getTime();
+            SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy kk:mm:ss");
+            String formattedDate = df.format(c);
+
+            bm.setFormBody("Bekci Denetimi: " + formattedDate);
+            bm.send();
+        } catch (Exception exp) {
+            Log.println(Log.ERROR, "Exp:", exp.toString());
+        }
+    }
 }
